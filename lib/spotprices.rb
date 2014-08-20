08784b659c7179c6b786a4bd133d69640df216b5
Eric Herot <eric.github@herot.com>
2014-08-20 15:23:27 -0400

    Clean up code to actually work

--- lib/spotprices.rb
+++ lib/spotprices.rb
@@ -1,28 +1,36 @@
 require "spotprices/version"
 
-module Spotprices
-  require 'fog'
+module EverTools
+  class SpotPrices
+    require 'fog'
 
-  def connection
-    @connection ||= Fog::Compute::AWS.new
-  end
-
-  def prices
-    p = connection.describe_spot_price_history.body['spotPriceHistorySet']
-    p.select do |s|
-      s['productDescription'] == 'Linux/UNIX (Amazon VPC)' &&
-        s['availabilityZone'] == 'us-east-1b'
+    def connection
+      @connection ||= Fog::Compute::AWS.new
     end
-  end
 
-  def display_header
-    printf('%-11s %8s', 'flavor', 'price')
-  end
+    def prices
+      p = connection.describe_spot_price_history.body['spotPriceHistorySet']
+      p = p.select do |s|
+        s['productDescription'] == 'Linux/UNIX (Amazon VPC)' &&
+          s['availabilityZone'] == 'us-east-1c'
+      end
+      output = []
+      p.map { |s| s['instanceType'] }.uniq.each do |s|
+        output << p.select { |spot| spot['instanceType'] == s }.first
+      end
+      output
+    end
 
-  def run
-    display_header
-    prices.each do |s|
-      printf('%s %s', s['instanceType'], s['spotPrice'])
+    def display_header
+      printf("%-11s %8s\n", 'Flavor', 'Price')
+      printf("%s\n", '-' * 20)
+    end
+
+    def run
+      display_header
+      prices.sort_by { |s| s['instanceType'] }.each do |s|
+        printf("%-11s %8s\n", s['instanceType'], s['spotPrice'])
+      end
     end
   end
 end
