d6286ac7630d4465bac55305e8158de26ba1c718
Eric Herot <eric.github@herot.com>
2014-08-21 11:23:06 -0400

    Formatting, averages, bug fixes

--- lib/spotprices.rb
+++ lib/spotprices.rb
@@ -1,18 +1,51 @@
 require "spotprices/version"
 
 module EverTools
+  class FlavorPrice
+    attr_accessor :prices
+
+    def latest
+      prices[prices.keys.sort.last]
+    end
+
+    def earliest
+      prices[prices.keys.sort.first]
+    end
+
+    def highest
+      prices.values.sort.last
+    end
+
+    def lowest
+      prices.values.sort.first
+    end
+
+    def mean
+      a = prices.values
+      (a.reduce(0.0, :+) / a.size).round(4)
+    end
+  end
+
   class SpotPrices
     require 'fog'
+    require 'colorize'
 
     def connection
       @connection ||= Fog::Compute::AWS.new
     end
 
+    def keyout(key, value, indent = 0)
+      print("#{' ' * indent}#{key.bold.black}: #{value}\n")
+    end
+
     def prices
-      p = connection.describe_spot_price_history.body['spotPriceHistorySet']
+      @prices ||= connection.describe_spot_price_history.body['spotPriceHistorySet']
+    end
+
+    def filtered_prices
       output = []
-      p.map { |s| s['productDescription'] }.uniq.each do |s_d|
-        p.map { |s| s['instanceType'] }.uniq.each do |s_i|
+      prices.map { |s| s['productDescription'] }.uniq.each do |s_d|
+        prices.map { |s| s['instanceType'] }.uniq.each do |s_i|
           output << p.select { |spot|
             spot['instanceType'] == s_i &&
               spot['productDescription'] == s_d
@@ -23,7 +56,7 @@ module EverTools
       output
     end
 
-    def display_header
+    def full_list_header
       output_header = format(
         '%-11s %-25s %8s',
         'Flavor',
@@ -34,8 +67,8 @@ module EverTools
       puts '-' * output_header.length
     end
 
-    def list
-      display_header
+    def full_list
+      full_list_header
       prices.sort { |a, b|
         if a['productDescription'] == b['productDescription']
           a['instanceType'] <=> b['instanceType']
@@ -52,8 +85,68 @@ module EverTools
       end
     end
 
+    def flavor_prices
+      prices.select { |spot| spot['instanceType'] == @flavor }
+    end
+
+    def products
+      if @flavor
+        flavor_prices.map { |s| s['productDescription'] }.uniq.sort
+      else
+        prices.map { |s| s['productDescription'] }.uniq.sort
+      end
+    end
+
+    def zones
+      if @flavor
+        flavor_prices.map { |s| s['availabilityZone'] }.uniq.sort
+      else
+        prices.map { |s| s['availabilityZone'] }.uniq.sort
+      end
+    end
+
+    def prices_by_time(flavor, zone, product)
+      output = {}
+      (@flavor? flavor_prices : prices).select do |p|
+        p['instanceType'] == flavor &&
+          p['availabilityZone'] == zone &&
+          p['productDescription'] == product
+      end.each do |p|
+        output[p['timestamp']] = p['spotPrice']
+      end
+      output
+    end
+
+    def one_type
+      keyout 'Instance Type', @flavor
+      products.each do |product|
+        keyout 'Product', product, 2
+        zones.each do |zone|
+          fp = FlavorPrice.new
+          fp.prices = prices_by_time(@flavor, zone, product)
+          unless fp.prices.empty?
+            keyout(
+              zone,
+              "(H#{fp.highest}/L#{fp.lowest}/M#{fp.mean}) " \
+                "#{Hash[fp.prices.sort.reverse].values.join(', ')}",
+              4
+            )
+          end
+        end
+      end
+    end
+
+    def initialize(args)
+      @args = args
+      @flavor = args[0] unless args.empty?
+    end
+
     def run
-      list if ARGV.empty?
+      if @args.empty?
+        full_list
+      else
+        one_type
+      end
     end
   end
 end
--- lib/spotprices/version.rb
+++ lib/spotprices/version.rb
@@ -1,3 +1,3 @@
 module Spotprices
-  VERSION = "1.0.2"
+  VERSION = "1.1.0"
 end
